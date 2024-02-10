949b8f5a5481c4f542a9197492ab93bed0132588
Jessie G <jessiel.gullo@gmail.com>
2024-02-09 22:28:58 -0500

    pizza type radio button

--- app/src/main/java/com/example/homework2/MainActivity.kt
+++ app/src/main/java/com/example/homework2/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.homework2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun pizzaTypeRadio(view: View) {
+        val selectedPizzaImg = when (view.id) {
+            R.id.bbq -> R.drawable.bbq_chicken
+            R.id.margherita -> R.drawable.margheritta //figure out how to spell margherita later
+            R.id.hawaiian -> R.drawable.hawaiian
+            else -> R.drawable.pepperoni
+        }
+        findViewById<ImageView>(R.id.imgPizza).setImageResource(selectedPizzaImg);
     }
 }
